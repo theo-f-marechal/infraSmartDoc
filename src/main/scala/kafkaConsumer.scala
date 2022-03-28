8b323def84fdda8cf0d3284e1e4d931413896f3b
theo <theo.francois.marechal@gmail.com>
2022-03-28 10:36:14 +0200

    more

--- src/main/scala/kafkaConsumer.scala
+++ src/main/scala/kafkaConsumer.scala
@@ -13,7 +13,7 @@ class kafkaConsumer(val topic: String) extends Logging {
 
   def createConsumerConfig(): Properties = {
     val props = new Properties()
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092")
     props.put(ConsumerConfig.GROUP_ID_CONFIG, "2")
     props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
     props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
